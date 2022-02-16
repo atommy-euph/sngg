058fa35c91c258478268bfd369d1f5c5ef0f5d86
arata <blacktommy314@gmail.com>
2022-02-16 12:14:35 +0900

    Add descriptionAdd description

--- src/components/modal/HowToPlayModal.tsx
+++ src/components/modal/HowToPlayModal.tsx
@@ -3,6 +3,7 @@ import {
   Modal,
   Heading,
   Text,
+  Link,
   Divider,
   HStack,
   useColorModeValue,
@@ -28,6 +29,15 @@ export const HowToPlayModal = React.memo(function HowToPlayModal({
   isOpen,
   onCloseHowToPlayModal,
 }: Props) {
+  const textProps = {
+    fontSize: 14,
+    my: 1,
+  };
+  const headingProps = {
+    fontSize: 14,
+    my: 1,
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onCloseHowToPlayModal}>
       <Modal.Content bg={useColorModeValue(lightBgColor, darkBgColor)}>
@@ -36,14 +46,12 @@ export const HowToPlayModal = React.memo(function HowToPlayModal({
           遊び方・ルール
         </Modal.Header>
         <Modal.Body>
-          <Heading fontSize={18} my={1}>
-            遊び方
-          </Heading>
-          <Text fontSize={14} my={1}>
+          <Heading {...headingProps}>遊び方</Heading>
+          <Text {...textProps}>
             {SIZE}文字の日本の駅・停留場の名前を{GUESS_MAX}
             回以内に当てるゲームです。
           </Text>
-          <Text fontSize={14} my={1}>
+          <Text {...textProps}>
             回答する駅名は{SIZE}
             文字で実際に存在する(リストに含まれる)必要があります。
           </Text>
@@ -144,7 +152,7 @@ export const HowToPlayModal = React.memo(function HowToPlayModal({
               value="ウ"
             />
           </HStack>
-          <Text fontSize={14} pt={2} pb={2}>
+          <Text pt={2} pb={4} fontSize={14}>
             <Text fontSize={14} pr={1} bold>
               キ
             </Text>
@@ -160,29 +168,44 @@ export const HowToPlayModal = React.memo(function HowToPlayModal({
           </Text>
 
           <Divider my={3} />
-          <Heading fontSize={18} my={1}>
-            ルール
-          </Heading>
+          <Heading {...headingProps}>ルール</Heading>
 
-          <Text fontSize={14} my={1}>
-            ・「駅」、「停留場」は文字数に含まない。5文字の場合「シブヤエキ」は解答できません。
+          <Text {...textProps}>
+            ・「駅」、「停留場」は文字数に含みません。5文字の場合「シブヤエキ」は解答できません。
           </Text>
-          <Text fontSize={14} my={1}>
-            ・数字、アルファベット表記はそのまま解答します。ただし、もちろん
+          <Text {...textProps}>
+            ・アルファベットや数字など、表記と読み方が異なる場合は表記を優先します。ただし、もちろん
             {SIZE}
-            文字以外の駅名は解答できません。例:「YRPノビ」「ニシ4チョウメ」
+            文字以外の駅名は解答できません。例:「YRPノビ」「ニシ4チョウメ」「ジヤトコマエ」
           </Text>
-          <Text fontSize={14} my={1}>
+          <Text {...textProps}>
             ・駅名に含まれる中黒、括弧は除きます。例:「栂・美木多」→「トガミキタ」
           </Text>
-          <Text>・</Text>
-          <Text>・</Text>
+          <Text>
+            ・駅名及びその読みは、
+            <Link
+              href="https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E9%89%84%E9%81%93%E9%A7%85%E4%B8%80%E8%A6%A7_%E3%81%82"
+              isExternal
+            >
+              Wikipedia「日本の鉄道駅一覧」
+            </Link>
+            に2022年2月16日時点で掲載されているものに準じます。
+          </Text>
           <Divider my={3} />
-          <Heading fontSize={18} my={1}>
-            このゲームについて
+          <Heading {...headingProps} color="gray.400" textAlign="center">
+            About this game.
           </Heading>
-          <Text>・</Text>
-          <Text>・</Text>
+          <Text color="gray.400" textAlign="center">
+            This game is inspired by{" "}
+            <Link
+              href="https://www.nytimes.com/games/wordle/index.html"
+              isExternal
+              _text={{ color: "gray.400" }}
+            >
+              Wordle
+            </Link>
+            .
+          </Text>
         </Modal.Body>
       </Modal.Content>
     </Modal>
