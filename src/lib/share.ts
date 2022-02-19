2d4b2011f89a1956cd2fd4a7fb04be1f69b5bc98
arata <blacktommy314@gmail.com>
2022-02-19 10:42:32 +0900

    add Twitter OGP

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
     `${GAME_TITLE} ${solutionIndex} ${
       lost ? 'X' : guesses.length
-    }/${GUESS_MAX}\n\n` + generateEmojiGrid(guesses)
+    }/${GUESS_MAX}\n\n` + generateEmojiGrid(guesses) + "\n\nrailword.com"
   )
 }
 
