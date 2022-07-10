24db148171e5d1bea57606c8606eaf5dd673aecd
dolphin6k <shin1@wmail.plala.or.jp>
2022-07-10 21:12:55 +0900

    Defined mywprintf() as wprintf() only for windows.

--- cli/tetsudoru.c
+++ cli/tetsudoru.c
@@ -7,10 +7,46 @@
 #include <time.h>
 #include <ctype.h>
 #include <locale.h>
+#include "data.h"
+
 #ifdef WIN32
 #include <windows.h>
+int mywprintf(const wchar_t *fmt, ...)
+{
+  wchar_t wstr[256];
+  va_list ap;
+
+  va_start(ap, fmt);
+  vswprintf(wstr, 255, fmt, ap);
+  va_end(ap);
+  int n;
+  char str[1024];
+  n = WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, 255, NULL, NULL);
+  str[n] = '\0';
+  printf(str);
+  return 0;
+}
+#define wprintf mywprintf
+
+void myputwc(wchar_t wch, FILE *fp)
+{
+  char str[5];
+  int n;
+  n = WideCharToMultiByte(CP_ACP, 0, &wch, 1, str, 3, NULL, NULL);
+  str[n] = '\0';
+  fprintf(fp, "%s", str);
+}
+#define putwc myputwc
 #endif
-#include "data.h"
+
+int isStation(wchar_t *str)
+{
+  int i;
+  for (i = 0; i < N; i++)
+    if (wcscmp(str, station[i]) == 0)
+      return 1;
+  return 0;
+}
 
 int isNumber(char *str)
 {
@@ -66,48 +102,6 @@ void compare(wchar_t *target, wchar_t *input, int *a)
 	    a[i] = b[j] = 3;
 }
 
-int mywprintf(const wchar_t *fmt, ...)
-{
-  wchar_t wstr[256];
-  va_list ap;
-
-  va_start(ap, fmt);
-  vswprintf(wstr, 255, fmt, ap);
-  va_end(ap);
-#ifdef WIN32
-  int n;
-  char str[1024];
-  n = WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, 255, NULL, NULL);
-  str[n] = '\0';
-  printf(str);
-#else
-  wprintf(wstr);
-#endif
-  return 0;
-}
-
-void mywputc(wchar_t wch)
-{
-#ifdef WIN32
-  char str[5];
-  int n;
-  n = WideCharToMultiByte(CP_ACP, 0, &wch, 1, str, 3, NULL, NULL);
-  str[n] = '\0';
-  printf("%s", str);
-#else
-  putwc(wch, stdout);
-#endif
-}
-
-int isStation(wchar_t *str)
-{
-  int i;
-  for (i = 0; i < N; i++)
-    if (wcscmp(str, station[i]) == 0)
-      return 1;
-  return 0;
-}
-
 int count_rest(wchar_t *problem, int nans)
 {
   int i, j, k, n, a[5], b[5];
@@ -134,13 +128,13 @@ int start_game(int day)
   wchar_t problem[6];
 
   wcscpy(problem, station[day]);
-  mywprintf(L"\n");
+  wprintf(L"\n");
   nans=0;
   while(1){
-    mywprintf(L" %d : \e[K", nans + 1);
+    wprintf(L" %d : \e[K", nans + 1);
     gets(input);
     if(strlen(input) == 0) {
-      mywprintf(L"\e[F");
+      wprintf(L"\e[F");
       continue;
     }
 #ifdef WIN32
@@ -149,31 +143,31 @@ int start_game(int day)
     mbstowcs(ans[nans], input, 6);
 #endif
     if (!isStation(ans[nans])){
-      mywprintf(L"\e[F %d : リストにありません\e[K\n", nans + 1);
+      wprintf(L"\e[F %d : リストにありません\e[K\n", nans + 1);
       sleep(1);
-      mywprintf(L"\e[F");
+      wprintf(L"\e[F");
       continue;
     }else{
       compare(problem, ans[nans], a);
-      mywprintf(L"\e[F %d : ", nans + 1);
+      wprintf(L"\e[F %d : ", nans + 1);
       for (i = 0; i < 5; i++){
-	mywprintf(L"%s", color[a[i]]);
-	mywputc(ans[nans][i]);
-	mywprintf(L"%s", color[0]);
+	wprintf(L"%s", color[a[i]]);
+	putwc(ans[nans][i], stdout);
+	wprintf(L"%s", color[0]);
       }
       if (wcscmp(problem, ans[nans]) == 0){
-	mywprintf(L"\n\n目的地に到着！\n");
+	wprintf(L"\n\n目的地に到着！\n");
 	return 0;
       }
       if (nans < 5){
 	rest=count_rest(problem, nans);
-	mywprintf(L" - 残り候補数: %3d", rest);
+	wprintf(L" - 残り候補数: %3d", rest);
       }
-      mywprintf(L"\n");
+      wprintf(L"\n");
       nans++;
       if (nans == 6) {
-	mywprintf(L"\n残念！途中下車…\n");
-	mywprintf(L"\n正解は「%ls」でした。\n", problem);
+	wprintf(L"\n残念！途中下車…\n");
+	wprintf(L"\n正解は「%ls」でした。\n", problem);
 	return 0;
       }
     }
@@ -201,18 +195,18 @@ int main(void)
   
   day_max = (t-t0)/86400+1;
   while(1){
-    mywprintf(L"\nゲーム番号を入力 [1-%d] (0で終了) : \e[K", day_max);
+    wprintf(L"\nゲーム番号を入力 [1-%d] (0で終了) : \e[K", day_max);
     gets(str);
     if(strlen(str) == 0 || !isNumber(str)) {
-      mywprintf(L"\e[2F");
+      wprintf(L"\e[2F");
       continue;
     }
     day = atoi(str);
     if (day == 0) return 0;
     if (day > day_max){
-      mywprintf(L"\n未来の問題はできません！");
+      wprintf(L"\n未来の問題はできません！");
       sleep(1);
-      mywprintf(L"\e[G\e[K\e[3F");
+      wprintf(L"\e[G\e[K\e[3F");
     }else{
       start_game(day);
     }
