ade7561bc56a66ae0aa35e6468f9169aadce121e
dolphin6k <dolphin6k@wmail.plala.or.jp>
2023-03-20 09:09:15 +0900

    Added perl script to remove an extinct station from the list

---
+++ src/constants/remove.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+# リストから指定した駅を削除する perl スクリプト
+# 過去に出題された駅を削除したら、src/lib/words.ts のdayOffsetの値を
+# インクリメントすること。
+#
+# 使い方： ./remove.pl < input > output
+
+# 削除する駅
+$extinct = "ハマタウラ";
+
+# 削除前のオフセット値
+$dayOffset = 1;
+
+$n = 0;
+
+while (<>)
+{
+    if (/^.+: \[$/){
+	$list[$n] = $_;
+	last;
+    }
+    $header .= $_;
+}
+
+while (<>){
+    if (/^\}\;$/){
+	$n--;
+	last;
+    }
+    $list[$n] .= $_;
+    if (/^.*\],$/){
+	if ($list[$n] =~ /$extinct/){
+	    $nrem = $n;
+	}
+	$n++;
+    }
+}
+
+print $header;
+for ($i = 0; $i <= $n; $i++){
+    if ($list[$i] =~ /$extinct/){
+	next;
+    }
+    print $list[$i];
+}
+print "};\n";
+
+$stnum = $nrem + $dayOffset + 1;
+print STDERR "Station No. $stnum - $extinct removed\n";
